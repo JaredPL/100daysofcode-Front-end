ada4af123752021f6c27f6e7d87066b843dc1d98
JaredPL <78654632+JaredPL@users.noreply.github.com>
2022-12-31 18:18:19 +0100

    Add files via upload

---
+++ Day48/script.js
@@ -0,0 +1,10 @@
+
+const star = document.querySelectorAll(".fa-star");
+
+star.forEach(starIcon =>{
+  eyeIcon.addEventListener("musover", (event) =>{
+    
+    event.target.style.color = "purple";
+
+    })
+  })
